f5b51697ee3c0c90ebe6faec67ca360c27fde220
Collin Heyden <collinheyden44@gmail.com>
2018-10-09 11:21:49 -0500

    Activity3

---
+++ src/oop/activ3.java
@@ -0,0 +1,16 @@
+package oop;
+
+public class activ3 {
+
+	public static void main(String[] args) {
+		Greeter bob = new Greeter();
+		Friend john = new Friend();
+		bob.sayHello();
+		bob.sayHello();
+		bob.sayHello();
+		john.sayMessage();
+		bob.sayHello();
+
+	}
+
+}
