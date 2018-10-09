fa5fb10d288c23bcfa3f2ace98665160df58655c
Collin Heyden <collinheyden44@gmail.com>
2018-10-09 11:22:07 -0500

    Activity4

---
+++ src/oop/activ4.java
@@ -0,0 +1,16 @@
+package oop;
+
+public class activ4 {
+
+	public static void main(String[] args) {
+		Friend jenny = new Friend();
+		Friend billy = new Friend();
+		Friend sammy = new Friend();
+		Friend kyle = new Friend();
+		billy.sayMessage();
+		kyle.sayMessage();
+
+
+	}
+
+}
