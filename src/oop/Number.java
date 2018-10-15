6f2756055eb7db7586b4f306da5c30f419edd19a
Collin Heyden <collinheyden44@gmail.com>
2018-10-15 09:50:32 -0500

    Number

--- src/oop/Number.java
+++ src/oop/Number.java
@@ -10,4 +10,16 @@ public class Number {
 		System.out.println("You sent " + num + " to the method");
 	}
 	
+	public void sayNumberPart2(double num) {
+		num = num + 2;
+		System.out.println(num);
+	}
+	
+	public void sayNum(double num, double num2) {
+		System.out.println(num + num2);
+	}
+	
+	public int returnRoundUp(double number) {
+		return (int)Math.ceil(number);
+	}
 }
