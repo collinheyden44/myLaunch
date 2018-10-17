2198a050cdea24ceaac834b3c07730f253447d35
Collin Heyden <collinheyden44@gmail.com>
2018-10-17 10:41:21 -0500

    CircleTest

--- src/oop/CircleTest.java
+++ src/oop/CircleTest.java
@@ -2,9 +2,20 @@ package oop;
 
 public class CircleTest {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		public static void main(String [] args){
 
-	}
-
+		Circle ball = new Circle();
+		System.out.println("ball radius:" + ball.returnRadius());
+		System.out.println("ball circumference:" + ball.circumference());
+		System.out.println("ball area:" + ball.area());
+		ball.changeRadius(7.2);
+		System.out.println("ball radius:" + ball.returnRadius());
+		ball = new Circle(12.25);
+		System.out.println("ball radius:" + ball.returnRadius());
+		System.out.println("ball circumference:" + ball.circumference());
+		System.out.println("ball area:" + ball.area());
+		ball.changeRadius(7.2);
+		System.out.println("ball radius:" + ball.returnRadius());
+		
+		}
 }
