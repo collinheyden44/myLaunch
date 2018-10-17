ec164fcca9707d1bbe7760493da28ee56953c23d
Collin Heyden <collinheyden44@gmail.com>
2018-10-17 09:09:14 -0500

    Greeter1

--- src/oop/Greeter.java
+++ src/oop/Greeter.java
@@ -2,14 +2,23 @@ package oop;
 
 public class Greeter {
 	
-	public Greeter() 
-	{
-
+	private int age;
+	
+	public Greeter() {
+		age = 98;
 	}
 	
-	public void sayHello()
-	{
+	public int getAge() {
+		return age;
+	}
+	
+	public void setAge(int newAge) {
+		age = newAge;
+	}
+	
+	
+	public void sayHello() {
 		System.out.print("Hello");
 	}
 	
-	}
+}
