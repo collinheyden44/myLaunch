dbb80133b0d58f18fdef04869c370908fe05a162
Collin Heyden <collinheyden44@gmail.com>
2018-10-18 16:28:14 -0500

    Cat

--- src/oop/Cat.java
+++ src/oop/Cat.java
@@ -2,4 +2,37 @@ package oop;
 
 public class Cat {
 
+	private int age;
+	private int sleep;
+	
+	public Cat() {
+		age = 12;
+		sleep = 12;
+	}
+	
+	public Cat(int newAge, int newSleep) {
+		age = newAge;
+		sleep = newSleep;
+	}
+	
+	public void birthday() {
+		age = age + 1;
+	}
+	
+	public void sleepMore() {
+		sleep = sleep + 1;
+	}
+	
+	public void sleepLess() {
+		sleep = sleep -1;
+	}
+	
+	public int returnAge() {
+		return age;
+	}
+	
+	public int returnSleep() {
+		return sleep;
+	}
+	
 }
