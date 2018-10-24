d6b668cabf5da496db2014f2581dc5bb72c07a47
Collin Heyden <collinheyden44@gmail.com>
2018-10-24 09:06:44 -0500

    RectangleTest

--- src/oop/RectangleTest.java
+++ src/oop/RectangleTest.java
@@ -8,16 +8,19 @@ public class RectangleTest {
 		System.out.println("Width: " + bob.returnWidth());
 		System.out.println("Perimeter: " + bob.perimeter());
 		System.out.println("Area: " + bob.area());
+		
 		bob = new Rectangle(10.0, 12.0);
 		System.out.println("\n" + "Length: " + bob.returnLength());
 		System.out.println("Width: " + bob.returnWidth());
 		System.out.println("Perimeter: " + bob.perimeter());
 		System.out.println("Area: " + bob.area());
+		
 		bob.changeLength(12);
 		System.out.println("\n" + "Length: " + bob.returnLength());
 		System.out.println("Width: " + bob.returnWidth());
 		System.out.println("Perimeter: " + bob.perimeter());
 		System.out.println("Area: " + bob.area());
+		
 		bob.changeWidth(20);
 		System.out.println("\n" + "Length: " + bob.returnLength());
 		System.out.println("Width: " + bob.returnWidth());
